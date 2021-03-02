123da7785fb593bee65aea5fc7ac3eef3b70574e
Ankit Gautam <0agautam@gmail.com>
2021-03-02 19:37:16 +0530

    Associations added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  has_and_belongs_to_many :carts
+  belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_one :cart
+  has_one :account
+  has_one :account_history, through: :account
+  has_many :orders
 end
