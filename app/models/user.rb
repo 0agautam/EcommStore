550cac9a3b98f2ffddaa77c2b7aab40e5865665e
Ankit Gautam <0agautam@gmail.com>
2021-03-03 21:22:44 +0530

    validation added presence, acceptance, validates_associated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   has_one :account
   has_one :account_history, through: :account
   has_many :orders
+
+#validations
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :address, presence: true
+
+  validates :terms_of_service, acceptance: {message:'Must be abided strictly',accept:['true','yes']}
+  validates :eula, acceptance: {message:'Must be abided', accept:['trusted','accepted']}
+
+  #association validaiton
+  validates_associated :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_112036) do
+ActiveRecord::Schema.define(version: 2021_03_03_130539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,11 +37,32 @@ ActiveRecord::Schema.define(version: 2021_03_02_112036) do
     t.integer "account_number"
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "product_id"
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts_products", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_carts_products_on_cart_id"
+    t.index ["product_id"], name: "index_carts_products_on_product_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -58,6 +79,15 @@ ActiveRecord::Schema.define(version: 2021_03_02_112036) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "email"
+    t.string "terms_of_service"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
@@ -65,7 +95,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_112036) do
     t.text "specs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "cart_id"
   end
 
   create_table "users", force: :cascade do |t|
