8ff8c3ea2d43343d787a722b9cf05ebc991c0fda
Ankit Gautam <0agautam@gmail.com>
2021-03-04 12:12:29 +0530

    custom validation: name, email using regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.name =~/\A[A-Z][a-z]+\z/
+      record.errors.add :base, "Name should contain only letters starting with Upper case"
+    end
+  end
+end
+
+class EmailValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.email =~ /\A([^@\s]+)@((?:[a-zA-Z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add :base, "Enter a valid email"
+    end
+  end
+end
+
 class User < ApplicationRecord
   has_one :cart
   has_one :account
@@ -5,7 +21,7 @@ class User < ApplicationRecord
   has_many :orders
 
 #validations
-  validates :name, presence: true, format: {with: /\A[A-Za-z]+\z/,message:"should contain alphabets only"}
+  #validates :name, presence: true, format: {with: /\A[A-Za-z]+\z/,message:"should contain alphabets only"}
   
   validates :address, presence: true
 
@@ -15,11 +31,12 @@ class User < ApplicationRecord
   #association validaiton
   validates_associated :orders
   
-  validates :email, presence: true, confirmation: {case_sensitive: false}, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, confirmation: {case_sensitive: false}, uniqueness: {case_sensitive: false},email: true
   validates :email_confirmation, presence: true
 
   validates :pincode, exclusion: {in: %w(100000 200000 300000),message:"%{value} is excluded"}, length: {is: 6},format: {with: /\A[0-9]+\z/, message:"only numbers allowed"}
   
-  validates :account, presence:true
+  validates_with MyValidator
+  validates_with EmailValidator
 end
 
